706d80cf6d2d13ed0e0e6728f5e63aac8058af38
Arthur Dantas <arthur.dantas.prt@gmail.com>
2021-04-21 10:55:55 -0300

    corrigido cartas

--- funcional/cartas.hs
+++ funcional/cartas.hs
@@ -1,8 +1,9 @@
 data Carta = Carta {
-    Nome :: String,
-    Ataque :: Int,
-    Defesa :: Int
+    nome :: String,
+    ataque :: Int,
+    defesa :: Int,
+    modoAtaque :: Bool
 } deriving (Show)
 
 programacao1 :: Carta
-programacao1 = Carta "Programação 1" 600 400 
+programacao1 = Carta "Programação 1" 600 400 True
