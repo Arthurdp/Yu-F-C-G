281aab7e402e560d82c6c6ff33f749be030e5d20
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2021-04-27 16:30:12 -0300

    cartas 1 ao 7
    criando cartas do p1 ao p7

--- funcional/cartas.hs
+++ funcional/cartas.hs
@@ -1,19 +1,93 @@
-data Carta = Carta {
-    id :: Int,
-    nome :: String,
-    ataque :: Int,
-    defesa :: Int,
-    modoAtaque :: Bool,
-    tipo :: String
-} deriving (Show)
+
+type Id = Int
+type Nome = String
+type Ataque = Int
+type Defesa = Int
+type ModoAtaque = Bool
+type Equips = [Carta]
+
+data Carta = Equipamento Id Nome Ataque Defesa | Aluno Id Nome Ataque Defesa ModoAtaque Equips | Monstro Id Nome Ataque Defesa ModoAtaque
+ deriving (Show)
 
 --Cartas Tipo Equipamento de Aluno
 
 cafezin :: Carta
-cafezin = Carta 37 "Cafezin" 600 500 True "Equip"
+cafezin = Equipamento 37 "Cafezin" 600 500 
 
 atestado :: Carta
-atestado = Carta 38 "atestado médico" 400 1000 True "Equip"
+atestado = Equipamento 38 "atestado médico" 400 1000 
 
 provaEm2 :: Carta
-provaEm2 = Carta 39 "Prova em dupla" 900 500 True "Equip"
+provaEm2 = Equipamento 39 "Prova em dupla" 900 500 
+
+--Cartas Monstro 1 2 periodo
+
+sePerdeu :: Carta
+sePerdeu = Monstro 40 "Se perdeu no Campus" 900 700 True
+
+python :: Carta
+python = Monstro 41 "Aprender python" 700 600 True
+
+fmcc1 :: Carta
+fmcc1 = Monstro 42 "FUND DE MATEMATICA P/ C.DA COMPUTACAO I" 800 800 True
+
+fmcc2 :: Carta
+fmcc2 = Monstro 43 "FUND DE MATEMATICA P/ C.DA COMPUTACAO II" 900 900 True
+
+calculo1 :: Carta
+calculo1 = Monstro 44 "calculo I" 900 850 True
+
+--Cartas Monstro 3 4 periodo
+
+calculo2 :: Carta
+calculo2 = Monstro 45 "calculo II" 1300 1000 True
+
+provas3 :: Carta
+provas3  = Monstro 46 "3 provas no mesmo dia" 1500 1200 True
+
+sono :: Carta
+sono  = Monstro 47 "Sono na aula" 1100 1000 True
+
+colaErrada :: Carta
+colaErrada = Monstro 48 "cola errada" 1150 1100 True
+
+grafos :: Carta
+grafos = Monstro 49 "Video aula de Grafos" 1250 1100 True
+
+--Cartas Monstro 5 6 periodo
+
+crise :: Carta
+crise = Monstro 50 "A crise do meio do curso" 1650 1600 True
+
+ead :: Carta
+ead = Monstro 51 "o curso agora é EAD" 1900 1800 True
+
+prazo :: Carta
+prazo = Monstro 52 "Trabalho com prazo curto" 1800 1700 True
+
+optativasDo6 :: Carta
+optativasDo6 = Monstro 53 "Escolher as optativa do 6periodo" 1600 1600 True
+
+perdido :: Carta
+perdido = Monstro 54 "Você está perdido no curso" 1900 1850 True
+
+--Cartas Monstro 7 8 periodo
+
+ressaca :: Carta
+ressaca = Monstro 55 "Ir para a aula de ressaca" 2100 2200 True
+
+trabalhoGrupo :: Carta
+trabalhoGrupo = Monstro 56 "Trabalho em grupo" 2150 2400 True
+
+
+
+
+
+
+
+
+
+
+
+
+
