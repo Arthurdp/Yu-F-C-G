3d508cf528a3f45b43852bb0e8303f79e018019d
Victor Paiva dos Santos <50140824+VictorPaivaSantos@users.noreply.github.com>
2021-04-28 23:13:44 -0300

    mais cartas adicionadas

--- funcional/cartas.hs
+++ funcional/cartas.hs
@@ -1,4 +1,5 @@
 data Carta = Carta {
+    id :: int,
     nome :: String,
     ataque :: Int,
     defesa :: Int,
@@ -7,3 +8,88 @@ data Carta = Carta {
 
 programacao1 :: Carta
 programacao1 = Carta "Programação 1" 600 400 True
+
+--Cartas 1 2 periodo
+
+matematicaBasica :: Carta
+matematicaBasica = Carta "Matemática básica" 700 500 True 
+
+empolgacao :: Carta
+empolgacao = Carta "Empolgação" 700 500 True 
+
+fome :: Carta
+fome = Carta "Fome" 700 500 True 
+
+perdeuOnibus :: Carta
+perdeuOnibus = Carta "Perdeu ônibus" 700 500 True 
+
+esquecimento :: Carta
+esquecimento = Carta "Esquecimento" 700 500 True 
+
+--Cartas 3 4 periodo
+
+bd1 :: Carta
+bd1 = Carta "Banco de dados 1" 700 500 True 
+
+preguica :: Carta
+preguica = Carta "Preguiça" 700 500 True 
+
+roubo :: Carta
+roubo = Carta "Roubo" 700 500 True 
+
+namoro :: Carta
+namoro = Carta "Namoro" 700 500 True 
+
+sagui :: Carta
+sagui = Carta "Sagui" 700 500 True 
+
+--Cartas 5 6 periodo
+
+pato :: Carta
+pato = Carta "Pato" 700 500 True
+
+gato :: Carta
+gato = Carta "Gato" 700 500 True 
+
+cachorro :: Carta
+cachorro = Carta "Cachorro" 700 500 True 
+
+metodologiaCientifica :: Carta
+metodologiaCientifica = Carta "Metodologia cientifica" 700 500 True 
+
+wifi :: Carta
+wifi = Carta "Wi-fi" 700 500 True 
+
+--Cartas 7 8 periodo
+
+sorte :: Carta
+sorte = Carta "Sorte" 700 500 True 
+
+amizade :: Carta
+amizade = Carta "Amizade" 700 500 True 
+
+vicio :: Carta
+vicio = Carta "Vício" 700 500 True 
+
+iguana :: Carta
+iguana = Carta "Iguana" 700 500 True 
+
+dorDeCabeca :: Carta
+dorDeCabeca = Carta "Dor de cabeça" 700 500 True 
+
+--Cartas 9 periodo
+
+laguinho :: Carta
+laguinho = Carta "Laguinho" 700 500 True
+
+greve :: Carta
+greve = Carta "Greve" 700 500 True
+
+fimDoMundo :: Carta
+fimDoMundo = Carta "Fim do Mundo" 700 500 True
+
+insonia :: Carta
+insonia = Carta "Insônia" 700 500 True 
+
+jacare :: Carta
+jacare = Carta "Jacaré" 700 500 True 
