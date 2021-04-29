744b550eae6cc74798a843f6d923df21759cef16
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2021-04-29 02:31:45 -0300

    Update cartas.hs

--- funcional/cartas.hs
+++ funcional/cartas.hs
@@ -1,22 +1,29 @@
-programacao1 :: Carta
-programacao1 = Carta "Programação 1" 600 400 True
+module Cartas where
+
+data Carta = Carta {
+    iD :: Int,
+    nome :: String,
+    ataque :: Int,
+    defesa :: Int,
+    modoAtaque :: Bool
+} deriving (Show)
 
 --Cartas 1 2 periodo
 
 matematicaBasica :: Carta
-matematicaBasica = 2 Carta "Matemática básica" 500 700 True 
+matematicaBasica = Carta 2  "Matemática básica" 500 700 True 
 
 empolgacao :: Carta
-empolgacao = 3 Carta "Empolgação" 900 600 True 
+empolgacao = Carta 3  "Empolgação" 900 600 True 
 
 fome :: Carta
 fome = Carta 4 "Fome" 800 500 True 
 
 perdeuOnibus :: Carta
-perdeuOnibus = 5 Carta "Perdeu ônibus" 600 750 True 
+perdeuOnibus = Carta 5  "Perdeu ônibus" 600 750 True 
 
 esquecimento :: Carta
-esquecimento = 6 Carta "Esquecimento" 800 700 True 
+esquecimento = Carta 6 "Esquecimento" 800 700 True 
 
 --Cartas 3 4 periodo
 
@@ -24,68 +31,68 @@ bd1 :: Carta
 bd1 = Carta 7 "Banco de dados 1" 1100 1300 True 
 
 preguica :: Carta
-preguica = 8 Carta "Preguiça" 1500 1200 True 
+preguica = Carta 8 "Preguiça" 1500 1200 True 
 
 roubo :: Carta
-roubo = 9 Carta "Roubo"  1250 1400 True 
+roubo = Carta 9 "Roubo"  1250 1400 True 
 
 namoro :: Carta
-namoro = 10 Carta "Namoro" 1400 1100 True 
+namoro = Carta 10 "Namoro" 1400 1100 True 
 
 sagui :: Carta
-sagui = 11 Carta "Sagui" 1100 1500 True 
+sagui = Carta 11 "Sagui" 1100 1500 True 
 
 --Cartas 5 6 periodo
 
 pato :: Carta
-pato = 37 Carta "Pato" 1700 1600 True
+pato = Carta 37 "Pato" 1700 1600 True
 
 gato :: Carta
-gato = 38 Carta "Gato" 2000 1600 True 
+gato = Carta 38 "Gato" 2000 1600 True 
 
 cachorro :: Carta
-cachorro = 39 Carta "Cachorro" 1700 2000 True 
+cachorro =Carta  39 "Cachorro" 1700 2000 True 
 
 metodologiaCientifica :: Carta
-metodologiaCientifica = 100 Carta "Metodologia cientifica" 1800 1700 True 
+metodologiaCientifica = Carta 100 "Metodologia cientifica" 1800 1700 True 
 
 wifi :: Carta
-wifi = 101 Carta "Wi-fi" 1600 1900 True 
+wifi = Carta 101 "Wi-fi" 1600 1900 True 
 
 --Cartas 7 8 periodo
 
 sorte :: Carta
-sorte = 102 Carta "Sorte" 2100 2200 True 
+sorte = Carta 102 "Sorte" 2100 2200 True 
 
 amizade :: Carta
-amizade = 103 Carta "Amizade" 2400 2200 True 
+amizade = Carta 103 "Amizade" 2400 2200 True 
 
 vicio :: Carta
-vicio = 104 Carta "Vício" 2500 2100 True 
+vicio = Carta 104 "Vício" 2500 2100 True 
 
 iguana :: Carta
-iguana = 105 Carta "Iguana" 2200 2500 True 
+iguana = Carta 105 "Iguana" 2200 2500 True 
 
 dorDeCabeca :: Carta
-dorDeCabeca = 106 Carta "Dor de cabeça" 2300 2000 True 
+dorDeCabeca = Carta 106 "Dor de cabeça" 2300 2000 True 
 
 --Cartas 9 periodo
 
 laguinho :: Carta
-laguinho = 107 Carta "Laguinho" 2500 2700 True
+laguinho = Carta 107 "Laguinho" 2500 2700 True
 
 greve :: Carta
-greve = 108 Carta "Greve" 2700 2600 True
+greve = Carta 108 "Greve" 2700 2600 True
 
 fimDoMundo :: Carta
-fimDoMundo = 109 Carta "Fim do Mundo" 2900 2600 True
+fimDoMundo = Carta 109 "Fim do Mundo" 2900 2600 True
 
 insonia :: Carta
-insonia = 110 Carta "Insônia" 2700 2500 True 
+insonia = Carta 110 "Insônia" 2700 2500 True 
 
 jacare :: Carta
-jacare = 110 Carta "Jacaré" 2800 2700 True 
-=======
+jacare = Carta 110 "Jacaré" 2800 2700 True 
+
 --Carta vazia
 
 cartaVazia :: Carta
