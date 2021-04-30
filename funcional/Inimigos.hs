afae2d97de755bf5a3dc09afc783865a68f5fab8
Victor Paiva dos Santos <50140824+VictorPaivaSantos@users.noreply.github.com>
2021-04-29 21:46:55 -0300

    Create Inimigos.hs

---
+++ funcional/Inimigos.hs
@@ -0,0 +1,35 @@
+module Inimigos where
+
+data Inimigo = Inimigo {
+    nomeInimigo :: String,
+    vidaInimigo :: Int,
+    deckInimigo :: [Carta]
+}
+
+periodo1 :: Inimigo
+periodo1 = "Primeiro período" 8000 []
+
+periodo2 :: Inimigo
+periodo2 = "Segundo período" 8000 []
+
+periodo3 :: Inimigo
+periodo3 = "Terceiro período" 8000 []
+
+periodo4 :: Inimigo
+periodo4 = "Quarto período" 8000 []
+
+periodo5 :: Inimigo
+periodo5 = "Quinto período" 8000 []
+
+periodo6 :: Inimigo
+periodo6 = "Sexto período" 8000 []
+
+periodo7 :: Inimigo
+periodo7 = "Sétimo período" 8000 []
+
+periodo8 :: Inimigo
+periodo8 = "Oitavo período" 8000 []
+
+periodo9 :: Inimigo
+periodo9 = "Nono período" 8000 []
+
