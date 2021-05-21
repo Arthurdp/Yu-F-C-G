3da243726a6276c1469bcee451b8039141d1354e
Arthur Dantas <arthur.dantas.prt@gmail.com>
2021-05-21 01:32:15 -0300

    Update batalha.pl

--- logico/batalha.pl
+++ logico/batalha.pl
@@ -1,61 +1,91 @@
 %batalha
 
 carta(1,'corno',1000).
-carta(2,'corno',10000).
+carta(10,'corno',10000).
+carta(100,'corno',10000).
+carta(00,'corno',10000).
 
 
 main :-
     Jog = ['awd',[1],[1,10,100,100,100,1,10,100,100,100,1,10,100,100,100,1,10,100,100,100],8000,[1,10,100,00,00],[1,10,100,00,00],[1]],
-    printCampo(8000,16356,[1,10,100,00,00],[],[]),
     retiraCarta([1,10,100,00,00],100,Ret),
     batalha(Jog,['awd',[],[1,10,100,100,100,1,10,100,100,100,1,10,100,100,100,1,10,100,100,100],800,[],[],[]],Jog1),
-    writeln(Ret).
+    writeln(Ret),
+    writeln(Jog1).
 
+draw5([A,B,C,D,E|T],Mao,Deck) :-
+    Mao = [A,B,C,D,E],
+    Deck = T.
 
+draw([A|T],Mao,Mao1,Deck) :-
+    trocaCartaVazia(Mao,00,A,Mao1),
+    Deck = T.
 
-batalha([NomeJ,ColJ,DeckJ,VidaJ,MaoJ,CampoJ,DerrJ],[NomeI,ColI,DeckI,VidaI,MaoI,CampoI,DerrI],Jog) :- 
+batalha([NomeJ,ColJ,DeckJ,VidaJ,MaoJ,CampoJ,DerrJ],[NomeI,ColI,DeckI,VidaI,MaoI,CampoI,DerrI],Jog) :- !,
+    CampoJ1 = [00,00,00,00,00],
+    CampoI1 = [00,00,00,00,00], 
     geraDeckEmbaralhado(DeckJ,DeckJEm),
     geraDeckEmbaralhado(DeckI,DeckIEm),
+    draw5(DeckJEm,MaoJ1,DeckDrawJ),
+    draw5(DeckIEm,MaoI1,DeckDrawI),
 
-
-    printCampo(VidaJ,VidaI,CampoI,CampoJ,MaoJ),
-    menuInvoc(MaoJ),
+    printCampo(VidaJ,VidaI,CampoI1,CampoJ1,MaoJ1),
+    menuInvoc(MaoJ1),
     read(Index),
-    retiraCartaIndex(MaoJ,Index,1,MaoJ1),
-    write(MaoJ1),
-    removeIndex(MaoJ1,2,0,MaoJ2),
-    write(MaoJ2).
+    Index1 is Index-1,
+    nth0(Index1,MaoJ1,Carta),
+    trocaCartaVazia(CampoJ1,Carta,CampoJ2),
+    retiraCartaIndex(MaoJ,Index,1,MaoJ2),
+    
+    printCampo(VidaJ,VidaI,CampoI1,CampoJ2,MaoJ2),
+    Jog = [NomeJ,ColJ,DeckDrawJ,VidaJ,MaoJ2,CampoJ2,DerrJ].
 
+trocaCartaVazia([],_,[]) :- !. 
+trocaCartaVazia([_|T],00,Carta,L1) :- L1 = [Carta|T].
+trocaCartaVazia([H|T],00,Carta,[H|L1]) :- trocaCartaVazia(T,00,Carta,L1).
 
+trocaCartaIndex([],_,_,[]) :- !. 
+trocaCartaIndex([_|T],X,X,Carta,L1) :- L1 = [Carta|T].
+trocaCartaIndex([H|T],X,N,Carta,[H|L1]) :- 
+    N1 is N + 1,
+    trocaCartaIndex(T,X,N1,Carta,L1). 
 
 retiraCartaIndex([],_,_,[]) :- !. 
-retiraCartaIndex([_|T],X,X,L1) :- L1 is [00|T].
+retiraCartaIndex([_|T],X,X,L1) :- L1 = [00|T].
 retiraCartaIndex([H|T],X,N,[H|L1]) :- 
     N1 is N + 1,
     retiraCartaIndex(T,X,N1,L1). 
 
 retiraCarta([],_,[]) :- !. 
-retiraCarta([X|T],X,L1) :- L1 is [00|T].
+retiraCarta([X|T],X,L1) :- L1 = [00|T].
 retiraCarta([H|T],X,[H|L1]) :- retiraCarta(T,X,L1). 
 
-menuInvocaCartas(Mao) :-
-    nth0(0,Mao,Mao1),
-    nth0(1,Mao,Mao2),
-    nth0(2,Mao,Mao3),
-    nth0(3,Mao,Mao4),
-    nth0(4,Mao,Mao5),
+printCarta(IdCarta):- 
+    IdCarta =:= 00, writeln('|------Slot Vazio------|'),!
+    ;
+    IdCarta < 10, carta(IdCarta, Nome, Ataque),
+    write('Id: '), write(IdCarta), write('   | '), write('Ataque: '), write(Ataque), write(' | '), write('Nome: '), write(Nome), writeln('  '),!
+    ;
+    IdCarta < 100, carta(IdCarta, Nome, Ataque),
+    write('Id: '), write(IdCarta), write('  | '), write('Ataque: '), write(Ataque), write(' | '), write('Nome: '), write(Nome), writeln('  '),!
+    ;
+    carta(IdCarta, Nome, Ataque),
+    write('Id: '), write(IdCarta), write(' | '), write('Ataque: '), write(Ataque), write(' | '), write('Nome: '), write(Nome), writeln('  '),!.
+
+menuInvoc(Mao) :- 
+    nth0(0,Mao,Mao1), nth0(1,Mao,Mao2), nth0(2,Mao,Mao3), nth0(3,Mao,Mao4), nth0(4,Mao,Mao5), 
     writeln(''),
     writeln('#-----------Menu de Invocacao-----------#'),
     writeln(''),
-    writeln('#-----------Estas sao as cartas da sua mao-----------#\n'),
+    writeln('#-----------Estas sao as cartas da sua mao-----------#'),
     writeln(''),
-    writeln('[1] -> '), write(Mao1),
-    writeln('[2] -> '), write(Mao2),
-    writeln('[3] -> '), write(Mao3),
-    writeln('[4] -> '), write(Mao4),
-    writeln('[5] -> '), write(Mao5),
+    write('[1] -> '), printCarta(Mao1),!,
+    write('[2] -> '), printCarta(Mao2),!,
+    write('[3] -> '), printCarta(Mao3),!,
+    write('[4] -> '), printCarta(Mao4),!,
+    write('[5] -> '), printCarta(Mao5),!,
     writeln(''),
-    writeln('Digite sua escolha: ').
+    writeln('Digite sua escolha: '),!.
 
     
 geraDeckEmbaralhado([],[]).
@@ -78,7 +108,7 @@ remove([X|T],X,T).
 remove([H|T],X,[H|L1]) :- remove(T,X,L1).
 
 removeIndex([],_,_,[]) :- !.
-removeIndex([H|T],X,X,L1) :- L1 is T.
+removeIndex([_|T],X,X,L1) :- L1 = T.
 removeIndex([H|T],X,N,[H|L1]) :-
     N1 is N + 1,
     removeIndex(T,X,N1,L1).
@@ -124,7 +154,6 @@ printCampoJogador(Cartas) :-
     writeln('').
 
 printMao(Cartas) :-
-    writeln(''),
     write('-------------------Sua Mao-------------------'),
     writeln(''),
     writeln('|||||||||||||||||||||||||||||||||||||||||||||'),
