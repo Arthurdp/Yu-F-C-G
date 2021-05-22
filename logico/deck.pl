979b6255614a61ba6870fc3c639da6c20a405d31
Victor Paiva dos Santos <50140824+VictorPaivaSantos@users.noreply.github.com>
2021-05-22 01:08:28 -0300

    Update deck.pl

--- logico/deck.pl
+++ logico/deck.pl
@@ -1,5 +1,3 @@
-main :- jog(Jogador), deck(Jogador).
-
 deck(Jogador):- exibeDeck(Jogador), opcoesDeck(OpcaoDeck), gerenciaDeck(OpcaoDeck, Jogador).
 
 exibeDeck(Jogador):- deckJogador(Jogador, Deck), printMDeck(), printCartas(Deck).
@@ -15,7 +13,7 @@ printMColecao():- writeln('        #   COLEÇÃO    #       '), nl.
 printCartas([]):- writeln('').
 printCartas([Cab|Cal]):- printCarta(Cab), printCartas(Cal).
 
-printCarta(IdCarta):- carta(IdCarta, Nome, Ataque,_),
+printCarta(IdCarta):- carta(IdCarta, Nome, Ataque),
     write('Id: '), write(IdCarta), write(' | '), write('Nome: '), write(Nome),
     write(' | '), write('Ataque: '), write(Ataque), writeln(' | ').
 
@@ -26,21 +24,40 @@ opcoesDeck(OpcaoDeck):- writeln('[1] -> vizualizar coleção. '),
 opcoesColecao(OpcaoColecao):- writeln('[1] -> adicionar carta ao deck '),
     writeln('[2] -> deck'), read(OpcaoColecao).
 
-gerenciaDeck(1, Jogador):- printMColecao(), exibeColecao(Jogador), opcoesColecao(OpcaoColecao), gerenciaColecao(OpcaoColecao, Jogador).
+removeAdd(Jogador,Carta, R):- Jogador = [_, Colecao, Deck, _,_,_,_], member(Carta, Deck),
+    add(Colecao, Carta, NovaColecao), remove(Deck, Carta, NovoDeck),
+    R = [_, NovaColecao, NovoDeck, _,_,_,_], nl,writeln('Carta removida do deck!'), nl.
 
-gerenciaDeck(2, _):- write('carta remove.').
+removeAdd(Jogador,_, Jogador):- nl, writeln('Você não possui esta carta.'), nl. 
 
-gerenciaDeck(3, _):- write('menu').
+gerenciaDeck(1, Jogador):- printMColecao(), exibeColecao(Jogador),
+    opcoesColecao(OpcaoColecao), gerenciaColecao(OpcaoColecao, Jogador).
+
+gerenciaDeck(2, Jogador):- writeln('Id da carta: '), read(Id), removeAdd(Jogador, Id, NovoJogador), deck(NovoJogador).
+
+gerenciaDeck(3, Jogador):- Jogador = [_, _, Deck, _,_,_,_], length(Deck, Len), Len < 20, nl, 
+    writeln('Deck incompleto, tenha 20 cartas no deck para voltar.'), nl, deck(Jogador).
+
+gerenciaDeck(_,Jogador):- nl, writeln('Opção inválida.'),nl, deck(Jogador).
 
 exibeColecao(Jogador):- colecaoJogador(Jogador, Colecao), printCartas(Colecao).
 
-gerenciaColecao(1, _):- write('carta add').
+addRemove(Jogador,Carta, R):- Jogador = [_, Colecao, Deck, _,_,_,_], member(Carta, Colecao),
+    add(Deck, Carta, NovoDeck), remove(Colecao, Carta, NovaColecao),
+    R = [_, NovaColecao, NovoDeck, _,_,_,_], nl, writeln('Carta adicionada ao deck!'), nl.
+
+addRemove(Jogador,_, Jogador):-nl,  writeln('Você não possui esta carta.'), nl. 
+
+gerenciaColecao(1, Jogador):- Jogador = [_, _, Deck, _,_,_,_], length(Deck, Len), Len =:= 20,nl,
+    writeln('Deck cheio, remova uma carta antes de adicionar outra.'),nl,  gerenciaDeck(1, Jogador).
+
+gerenciaColecao(1, Jogador):- writeln('Id da carta: '), read(Id),
+    addRemove(Jogador,Id, NovoJogador),deck(NovoJogador).
 
 gerenciaColecao(2, Jogador):- deck(Jogador).
 
-jog(Jogador):- Jogador = ['exemplo', [2, 3], [1], 8000, [], [], []].  
+gerenciaColecao(_,Jogador):- nl, writeln('Opção inválida.'),nl, gerenciaDeck(1, Jogador).
 
-carta(1, 'p1', 100, 0).
-carta(2, 'p2', 200, 0).
-carta(3, 'p3', 300, 0).
-carta(4, 'p4', 400, 0).
+add(L,X,[X|L]).
+
+
